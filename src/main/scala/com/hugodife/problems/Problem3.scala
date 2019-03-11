47ffb3acd5e02b287d9e39fa4db555a48f4357c8
Hugo <hugodife@gmail.com>
2019-03-11 20:04:09 +0100

    Last solution

--- src/main/scala/com/hugodife/problems/Problem3.scala
+++ src/main/scala/com/hugodife/problems/Problem3.scala
@@ -2,10 +2,6 @@ package com.hugodife.problems
 
 object Problem3 {
   def solution(a: Array[Int]): Int = {
-    var total = 0
-    for(i <- a) {
-      total += i.toBinaryString.count(_ == '1')
-    }
-    total
+    a.map(_.toBinaryString.count(_ == '1')).sum
   }
 }
--- src/test/scala/com/hugodife/problems/Problem3Test.scala
+++ src/test/scala/com/hugodife/problems/Problem3Test.scala
@@ -9,6 +9,7 @@ class Problem3Test extends FunSuite with Matchers {
 
   test("All valid answers") {
     Problem3.solution(Array(1)) should equal(1)
+    Problem3.solution(Array(1)) should equal(1)
     Problem3.solution(Array(0, 1)) should equal(1)
     Problem3.solution(Array(1, 1)) should equal(2)
     Problem3.solution(Array(1, 5, 6)) should equal(5)
