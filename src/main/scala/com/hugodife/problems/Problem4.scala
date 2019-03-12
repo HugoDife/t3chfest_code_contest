9befd84dcb9a3e022ee17d396bbbdcd750930ec7
Hugo <hugodife@gmail.com>
2019-03-12 10:28:30 +0100

    Fixing errors

--- src/main/scala/com/hugodife/problems/Problem4.scala
+++ src/main/scala/com/hugodife/problems/Problem4.scala
@@ -12,6 +12,7 @@ object Problem4 {
 
   def solution(a: Array[Int]): Int = {
     if(a.length == 0) return 0
+    if(a.length == 1) return 2
     val intervals = a.sliding(2).map{ case Array(first, second) => second - first }.toArray
     (0 to intervals.length)
       .map(slice => computeValue(slice * price1D, intervals.drop(slice)))
@@ -19,22 +20,24 @@ object Problem4 {
   }
 
   def computeValue(acc: Int, intervals: Array[Int]): Int = {
-    if(intervals.length == 0) return 0
+    if(intervals.length < min7GroupIntervalLength) return acc + intervals.length * 2
     if(acc >= price30D) return price30D
+
     val groupIndex = first7DayGroupIndex(intervals).getOrElse(return acc + intervals.length * 2)
     val groupEndIndex = groupIndex + endIndexOf7DayGroup(intervals.drop(groupIndex))
+
     (groupEndIndex to intervals.length)
       .map(slice => {
         val previousBeforeGroup = groupIndex * price1D
         val priceAfterGroup = (slice - groupEndIndex) * price1D
-        computeValue(price7D + previousBeforeGroup + priceAfterGroup, intervals.drop(slice))
+        computeValue(acc + price7D + previousBeforeGroup + priceAfterGroup, intervals.drop(slice + 1))
       }).min
   }
 
   def first7DayGroupIndex(intervals: Array[Int]): Option[Int] = {
     //isGroup if min 3 intervals and max == 6
-    for(i <- 0 to (intervals.length - 3)) {
-      if(intervals.slice(i, min7GroupIntervalLengthExclusive).sum <= max7GroupIntervals)
+    for(i <- 0 to (intervals.length - min7GroupIntervalLength)) {
+      if(intervals.slice(i, i + min7GroupIntervalLength).sum <= max7GroupIntervals)
         return Some(i)
     }
     None
--- src/test/scala/com/hugodife/problems/Problem4Test.scala
+++ src/test/scala/com/hugodife/problems/Problem4Test.scala
@@ -20,4 +20,18 @@ class Problem4Test extends FunSuite with Matchers {
     Problem4.solution(Array(1, 4, 5, 7, 8, 10, 29, 30)) should equal(13)
     Problem4.solution(Array(1, 3, 4, 5, 7, 8, 9, 11)) should equal(11)
   }
+
+  test("Group checker") {
+    Problem4.first7DayGroupIndex(Array(1)) should equal(None)
+    Problem4.first7DayGroupIndex(Array(20)) should equal(None)
+    Problem4.first7DayGroupIndex((1 to 7).toArray) should equal(Some(0))
+    Problem4.first7DayGroupIndex((1 to 30).toArray) should equal(Some(0))
+    Problem4.first7DayGroupIndex(Array(1, 1, 6, 1, 1, 1)) should equal(Some(3))
+  }
+
+  test("End group checker") {
+    Problem4.endIndexOf7DayGroup(Array(1, 1, 1, 1)) should equal(3)
+    Problem4.endIndexOf7DayGroup(Array(1, 2, 2, 1, 1, 1)) should equal(3)
+    Problem4.endIndexOf7DayGroup(Array(1, 1, 1, 1, 1, 1, 1)) should equal(5)
+  }
 }
