7c8d4b73cbfb46ae8cd693217648401e44aaeb07
Hugo <hugodife@gmail.com>
2019-03-12 12:04:59 +0100

    From intervals to days and more functional

--- src/main/scala/com/hugodife/problems/Problem4.scala
+++ src/main/scala/com/hugodife/problems/Problem4.scala
@@ -2,8 +2,8 @@ package com.hugodife.problems
 
 object Problem4 {
 
-  val min7GroupIntervalLength = 3
-  val min7GroupIntervalLengthExclusive = min7GroupIntervalLength + 1
+  val min7DayLength = 4
+  val min7DayLengthExclusive = min7DayLength + 1
   val max7GroupIntervals = 6
 
   val price1D = 2
@@ -13,55 +13,45 @@ object Problem4 {
   def solution(a: Array[Int]): Int = {
     if(a.length == 0) return 0
     if(a.length == 1) return price1D
-    val intervals = a.sliding(2).map{ case Array(first, second) => second - first }.toArray
-    (0 to intervals.length)
-      .map(slice => computeValue(slice * price1D, intervals.drop(slice)))
+
+    a.indices
+      .map(slice => computeValue(slice * price1D, a.drop(slice)))
       .min
   }
 
-  def computeValue(acc: Int, intervals: Array[Int]): Int = {
-    if(intervals.length < min7GroupIntervalLength) return acc + intervals.length * price1D
-    if(acc >= price30D) return price30D
+  def computeValue(acc: Int, days: Array[Int]): Int = {
+      if(days.length < min7DayLength) return acc + days.length * price1D
+      if(acc >= price30D) return price30D
 
-    val groupIndex = first7DayGroupIndex(intervals).getOrElse(return acc + intervals.length * price1D)
-    val groupEndIndex = groupIndex + endIndexOf7DayGroup(intervals.drop(groupIndex))
+      val groupIndex = first7DayGroupIndex(days).getOrElse(return acc + days.length * price1D)
+      val groupEndIndex = groupIndex + endIndexOf7DayGroup(days.drop(groupIndex))
 
-    (groupEndIndex to intervals.length)
-      .map(slice => {
-        val previousBeforeGroup = groupIndex * price1D
-        val priceAfterGroup = (slice - groupEndIndex) * price1D
-        computeValue(acc + price7D + previousBeforeGroup + priceAfterGroup, intervals.drop(slice + 1))
-      }).min
+      (groupEndIndex to days.length)
+        .map(slice => {
+          val priceBeforeGroup = groupIndex * price1D
+          val priceAfterGroup = (slice - groupEndIndex) * price1D
+          computeValue(acc + price7D + priceBeforeGroup + priceAfterGroup, days.drop(slice + 1))
+        }).min
   }
 
-  def first7DayGroupIndex(intervals: Array[Int]): Option[Int] = {
-    //Is a 7 day group if min 3 intervals and max value = 6
-    val groupAndIndex = intervals.sliding(3).zipWithIndex.find{case (arr, ind) => is7DayGroup(arr)}
+  def first7DayGroupIndex(days: Array[Int]): Option[Int] = {
+    val groupAndIndex = days.sliding(min7DayLength).zipWithIndex.find{case (arr, ind) => is7DayGroup(arr)}
     val (group, index) = groupAndIndex.getOrElse(return None)
     Some(index)
-
-//    for(i <- 0 to (intervals.length - min7GroupIntervalLength)) {
-//      if(intervals.slice(i, i + min7GroupIntervalLength).sum <= max7GroupIntervals)
-//        return Some(i)
-//    }
-//    None
   }
 
-  private def is7DayGroup(interval: Array[Int]): Boolean = {
-    interval.length >= min7GroupIntervalLength && interval.sum <= max7GroupIntervals
+  private def is7DayGroup(days: Array[Int]): Boolean = {
+    days.length >= min7DayLength && (days.last - days.head) <= max7GroupIntervals
   }
 
-  def endIndexOf7DayGroup(intervals: Array[Int]): Int = {
-    var index = min7GroupIntervalLength
-    var intervalSum = intervals.slice(0, min7GroupIntervalLengthExclusive).sum
-    for(interval <- intervals.slice(min7GroupIntervalLengthExclusive, intervals.length)) {
-      intervalSum += interval
-      if(intervalSum > max7GroupIntervals)
-        return index
-      else
-        index += 1
+  def endIndexOf7DayGroup(days: Array[Int]): Int = {
+    var totalInterval = days(3) - days(0)
+
+    for(i <- 4 until days.length) {
+      if((days(i) - days.head) > 6)
+        return i - 1
     }
-    index
+    days.length - 1
   }
 }
 
--- src/test/scala/com/hugodife/problems/Problem4Test.scala
+++ src/test/scala/com/hugodife/problems/Problem4Test.scala
@@ -7,23 +7,18 @@ import org.scalatest.{FunSuite, Matchers}
 @RunWith(classOf[JUnitRunner])
 class Problem4Test extends FunSuite with Matchers {
 
-//  test("All basic cases") {
-//    Problem4.solution(Array()) should equal(0)
-//    Problem4.solution(Array(1)) should equal(2)
-//    Problem4.solution(Array(20)) should equal(2)
-//    Problem4.solution((1 to 7).toArray) should equal(7)
-//    Problem4.solution((1 to 30).toArray) should equal(25)
-//  }
-//
-//  test("More complex") {
-//    Problem4.solution(Array(1, 2, 4, 5, 7, 29, 30)) should equal(11)
-//    Problem4.solution(Array(1, 4, 5, 7, 8, 10, 29, 30)) should equal(13)
-//    Problem4.solution(Array(1, 3, 4, 5, 7, 8, 9, 11)) should equal(11)
-//    Problem4.solution(Array(1, 3, 4, 5, 7, 15, 16, 17, 20)) should equal(14)
-//  }
+  test("All basic cases") {
+    Problem4.solution(Array()) should equal(0)
+    Problem4.solution(Array(1)) should equal(2)
+    Problem4.solution(Array(20)) should equal(2)
+    Problem4.solution((1 to 7).toArray) should equal(7)
+    Problem4.solution((1 to 30).toArray) should equal(25)
+  }
 
-  test("Mo11re complex") {
-    val input = Array(1, 3, 4, 5, 7, 15, 16, 17, 20).sliding(2).map{ case Array(first, second) => second - first }.toArray
+  test("More complex") {
+    Problem4.solution(Array(1, 2, 4, 5, 7, 29, 30)) should equal(11)
+    Problem4.solution(Array(1, 4, 5, 7, 8, 10, 29, 30)) should equal(13)
+    Problem4.solution(Array(1, 3, 4, 5, 7, 8, 9, 11)) should equal(11)
     Problem4.solution(Array(1, 3, 4, 5, 7, 15, 16, 17, 20)) should equal(14)
   }
 
@@ -32,12 +27,12 @@ class Problem4Test extends FunSuite with Matchers {
     Problem4.first7DayGroupIndex(Array(20)) should equal(None)
     Problem4.first7DayGroupIndex((1 to 7).toArray) should equal(Some(0))
     Problem4.first7DayGroupIndex((1 to 30).toArray) should equal(Some(0))
-    Problem4.first7DayGroupIndex(Array(1, 1, 6, 1, 1, 1)) should equal(Some(3))
+    Problem4.first7DayGroupIndex(Array(1, 2, 8, 9, 10, 11)) should equal(Some(2))
   }
 
   test("End group checker") {
-    Problem4.endIndexOf7DayGroup(Array(1, 1, 1, 1)) should equal(3)
-    Problem4.endIndexOf7DayGroup(Array(1, 2, 2, 1, 1, 1)) should equal(3)
-    Problem4.endIndexOf7DayGroup(Array(1, 1, 1, 1, 1, 1, 1)) should equal(5)
+    Problem4.endIndexOf7DayGroup(Array(1, 2, 3, 4)) should equal(3)
+    Problem4.endIndexOf7DayGroup(Array(1, 3, 5, 6, 7, 8)) should equal(4)
+    Problem4.endIndexOf7DayGroup((1 to 9).toArray) should equal(6)
   }
 }
